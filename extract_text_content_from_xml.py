f457a236c9bca0e54e23e24d253ea045cb18e2e6
Omar Abuhashish <87930068+omarabuhash@users.noreply.github.com>
2023-07-11 16:44:22 -0400

    Add files via upload

---
+++ extract_text_content_from_xml.py
@@ -0,0 +1,63 @@
+import os
+import time
+import lxml
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+def get_urls(sitemap_url):
+    response = requests.get(sitemap_url)
+    xml_content = response.content
+    soup = BeautifulSoup(xml_content, 'lxml')
+    loc_elements = soup.find_all('loc')
+    urls = [loc.text for loc in loc_elements]
+    return urls
+
+def retry_get(driver, url):
+    retry_count = 5
+    retry_delay = 2
+    attempts = 0
+    while attempts < retry_count:
+        try:
+            driver.get(url)
+            return
+        except WebDriverException:
+            attempts += 1
+            time.sleep(retry_delay)
+    raise Exception(f"Failed to get URL: {url}")
+
+def run(url, driver, output_dir):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    try:
+        retry_get(driver, url)
+        time.sleep(0.5)
+        element = driver.find_element(By.ID, "provider-docs-content")
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        filepath = os.path.join(output_dir, url.split("/")[-1] + ".txt")
+        with open(filepath, "w") as file:
+            file.write(f"SOURCE FOR THE FILE: {url}\n\n")
+            file.write(element.text)
+    except Exception as e:
+        print(url)
+        print(e)
+
+def write_core_content_to_file(sitemap_urls, output_dir):
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    urls = []
+    for sitemap_url in sitemap_urls:
+        urls.extend(get_urls(sitemap_url))
+    for url in urls:
+        run(url, driver, output_dir)
+        time.sleep(0.5)
+    driver.quit()
+
+if __name__ == "__main__":
+    sitemap_urls = ["https://registry.terraform.io/sitemaps/providers-2.xml"]
+    output_dir_name = "docs"
+    write_core_content_to_file(sitemap_urls, output_dir_name)
